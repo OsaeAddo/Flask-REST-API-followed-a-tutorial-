df4eb1cd3495dcd25848ffc70ad1bfef95c37f0a
OsaeAddo <immanueloadd@gmail.com>
2021-07-16 09:46:49 +0000

    Add player id, name of the player & position to define player model

--- football_api/models/player.py
+++ football_api/models/player.py
@@ -1,4 +1,13 @@
 from football_api.database import db
 
 class Player(db.Model):
-    pass
+    """
+    Player Flask-SQLAlchemy Model
+    Represents objects contained in the players table
+    """
+    __tablename__ = "players"
+
+    player_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(), nullable=True)
+    position = db.Column(db.String(), nullable=False)
+    

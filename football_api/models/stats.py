0370d18896684175f739f99dbef3a88c10193848
OsaeAddo <immanueloaddo@gmail.com>
2021-07-16 17:03:07 +0000

    Add stats id for primary key

--- football_api/models/stats.py
+++ football_api/models/stats.py
@@ -1,4 +1,8 @@
 from football_api.database import db
 
 class Stats(db.Model):
-    pass
+    """
+    Stats Flask-SQLAlchemy Model
+    Represents objects contained in the stats table
+    """
+    stat_id = db.Column(db.Integer, primary_key=True, autoincrement=True)

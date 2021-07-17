2bb14ff9ce57818a06efeda0d26fd62ec714be22
OsaeAddo <immanueloaddo@gmail.com>
2021-07-17 18:42:24 +0000

    Add fields for team id, name & abbreviations

--- football_api/models/team.py
+++ football_api/models/team.py
@@ -6,3 +6,6 @@ class Team(db.Model):
     Represents objects contained in the teams table
     """
     ___tablename__ = "teams"
+    team_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(),unique=True, nullable=False)
+    abbreviations = db.Column(db.String(), unique=True, nullable=False)
